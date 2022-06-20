41e4d427b9d1ed2898942c6a4c6cb552536b98d5
LucasRM <lucasmoraes@xbrain.com.br>
2022-06-20 10:16:04 -0300

    Criando projeto, limpando e atualizando dependencias

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,33 @@
+const Navbar = () =>  {
+    return (
+        <nav className="bg-primary">
+        <div>
+          <a href="link">
+            <h4>Healthy Foods</h4>
+          </a>
+          <div>
+            <ul>
+              <li>
+                <a href="link">HOME</a>
+              </li>
+              <li>
+                <a href="link">DIETA</a>
+              </li>
+              <li>
+                <a href="link">REFEIÇÃO</a>
+              </li>
+              <li>
+                <a href="link">ALIMENTOS</a>
+              </li>
+              <li>
+                <a href="link">ADMIN</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      
+    );
+  }
+  
+  export default Navbar;
