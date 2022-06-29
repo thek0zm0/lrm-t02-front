32d58be8959799707c77e40fd541c82c810cafad
LucasRM <lucasmoraes@xbrain.com.br>
2022-06-29 10:54:09 -0300

    Configurando estilos displays pequenos

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const ButtonIcon = () => {
+    return (
+        <button className="btn btn-primary btn-icon">
+            <h6>Buscar Alimentos</h6>
+        </button>
+    );
+  }
+  
+  export default ButtonIcon;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/mainimage.svg';
+import ButtonIcon from 'components/ButtonIcon';
 import Navbar from "components/Navbar";
 import './styles.css'
 
@@ -10,6 +11,8 @@ const Home = () => {
             <div className="home-card">
                 <div className="home-content-container">
                     <h1>Comece agora uma vida mais saudável!</h1>
+                    <p>Conheça a importância da escolha de alimentos saudáveis na sua dieta.</p>
+                    <ButtonIcon></ButtonIcon>
                 </div>
                 <div className="home-image-container">
                     <MainImage></MainImage>
