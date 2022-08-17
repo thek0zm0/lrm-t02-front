ac78208642d41e6bb76f8555210058f9c0b31225
LucasRM <lucasmoraes@xbrain.com.br>
2022-08-17 08:51:47 -0300

    Funcao para verificar autenticado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,13 @@
 import { ReactComponent as MainImage } from 'assets/images/mainimage.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { getTokenData } from 'util/Requests';
+import { isAuthenticated } from 'util/Requests';
 import './styles.css'
 
 const Home = () => {
     return (
         <div className="home-container">
-            <h1>{getTokenData()?.user_name}</h1>
+            <h1>{isAuthenticated() ? 'sim' : 'nao'}</h1>
             <div className="base-card home-card">
                 <div className="home-content-container">
                     <div>
