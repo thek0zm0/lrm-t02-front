f83dc69da148698be02d6d547a0bcbf37c96e3e4
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2022-09-27 21:41:30 -0300

    estilo formulario de cadastro alimento

--- src/pages/Admin/Foods/Form/index.tsx
+++ src/pages/Admin/Foods/Form/index.tsx
@@ -7,18 +7,31 @@ const Form = () => {
             <div className="base-card food-crud-form-card">
                 <h1 className="food-crud-form-title">Informações do Alimento</h1>
                 <form action=''>
-                    <div className="row">
-                        <div className='col-lg-6'>
-                            <input className="base-input form-control"></input>
-                            <input className="base-input form-control"></input>
-                            <input className="base-input form-control"></input>
+                    <div className="row food-crud-inputs-container">
+                        <div className='col-lg-6 food-crud-inputs-left-container'>
+                            <div className='margin-botton-30'>
+                                <input className="base-input form-control"></input>
+                            </div>
+                            <div className='margin-botton-30'>
+                                <input className="base-input form-control"></input>
+                            </div>
+                            <div className='margin-botton-30'>
+                                <input className="base-input form-control"></input>
+                            </div>
+                            <div>
+                                <input className="base-input form-control"></input>
+                            </div>
                         </div>
                         <div className='col-lg-6'>
-                            <textarea name='' rows={10} className="base-input form-control"></textarea>
+                            <div>
+                                <textarea name='' rows={10} className="base-input form-control h-auto"></textarea>
+                            </div>
                         </div>
                     </div>
-                        <button className="btn-outline-danger">Voltar</button>
-                        <button className="btn-outline-primary">Salvar</button>
+                    <div className='food-crud-buttons-container'>
+                        <button className="btn-outline-danger food-crud-button">Voltar</button>
+                        <button className="btn-outline-primary food-crud-button">Salvar</button>
+                    </div>
                 </form>
             </div>
         </div>
